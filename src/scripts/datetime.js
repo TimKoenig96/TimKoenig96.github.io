9061469718d098c5c327e26d95be0276adc696c3
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2025-03-01 13:02:11 +0100

    edit: Starting date-time update moved

--- src/scripts/datetime.js
+++ src/scripts/datetime.js
@@ -1,6 +1,10 @@
 const dateTimeElement = document.getElementById("datetime");
 
-export function updateDateTime() {
+export function startDateTimeUpdater() {
+	updateDateTime();
+}
+
+function updateDateTime() {
 	const now = new Date();
 	const date = now.toLocaleDateString(undefined, { day: "2-digit", month: "2-digit", year: "numeric" });
 	const time = now.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit" });
