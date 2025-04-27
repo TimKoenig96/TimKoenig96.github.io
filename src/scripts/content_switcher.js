b681a590f1d85ce8390f208ebece31168d94fd2b
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2025-04-27 19:59:40 +0200

    add search param to load requested content on init

--- src/scripts/content_switcher.js
+++ src/scripts/content_switcher.js
@@ -1,23 +1,54 @@
 let currentContent = "about_me";
 
-function navbarClickHandler(event) {
-
-	// Get clicked button
-	const target = event.target.closest("[data-target]")?.dataset.target;
+function switchToContent(targetId) {
 
 	// No button clicked or active button clicked
-	if (!target || currentContent === target) return;
+	if (!targetId || currentContent === targetId) return;
 
 	// Deactivate current button and hide content
 	document.querySelector(`[data-target="${currentContent}"]`).classList.remove("active");
 	document.querySelector(`[data-content="${currentContent}"]`).classList.remove("active");
 
 	// Activeate target
-	document.querySelector(`[data-target="${target}"]`).classList.add("active");
-	document.querySelector(`[data-content="${target}"]`).classList.add("active");
+	document.querySelector(`[data-target="${targetId}"]`).classList.add("active");
+	document.querySelector(`[data-content="${targetId}"]`).classList.add("active");
 
 	// Update current content name
-	currentContent = target;
+	currentContent = targetId;
+}
+
+function navbarClickHandler(event) {
+
+	// Get clicked button
+	const targetId = event.target.closest("[data-target]")?.dataset.target;
+
+	// Switch to content
+	switchToContent(targetId);
+
+	// Update search parameters
+	const newUrl = new URL(document.location.href);
+	newUrl.searchParams.set("page", encodeURIComponent(targetId));
+	history.pushState({}, "", newUrl);
+}
+
+export function searchParamCheck() {
+
+	// Get search params and target page
+	const searchParams = new URLSearchParams(window.location.search);
+	const targetId = decodeURIComponent(searchParams.get("page"));
+
+	// Nothing specified
+	if (!targetId) return;
+
+	// Get button and content
+	const button = document.querySelector(`[data-target="${targetId}"]`);
+	const content = document.querySelector(`[data-content="${targetId}"]`);
+
+	// If button or content don't exist
+	if (!button || !content) return;
+
+	// Switch to content
+	switchToContent(targetId);
 }
 
 export function setupContentSwitching() {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-import { setupContentSwitching } from "./content_switcher.js";
+import { setupContentSwitching, searchParamCheck } from "./content_switcher.js";
 import { startDateTimeUpdater } from "./datetime.js";
 import { setupNavbarToggle } from "./navbar_toggler.js";
 import { generateTimeline } from "./timeline.js";
@@ -16,6 +16,9 @@ function init() {
 
 	// Setup content switching via navbar buttons
 	setupContentSwitching();
+
+	// Get and switch to content if specified
+	searchParamCheck();
 }
 
 document.addEventListener("DOMContentLoaded", init);
